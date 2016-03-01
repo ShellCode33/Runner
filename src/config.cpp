c02e8346aa574693cf7aaa36ebb2a3027669cc4e
ShellCode <clement.fleury33@gmail.com>
2016-03-01 14:01:42 +0100

    Lecture de la config

--- src/config.cpp
+++ src/config.cpp
@@ -14,28 +14,46 @@ Config::~Config()
 
 void Config::createConfig()
 {
-    fstream config_file("config", ios::out | ios::in);
+    fstream config_file(filename, ios::out);
 
     if(config_file.is_open())
     {
-        config_file << "#Config File" << endl;
+        config_file << "resolution=1920x1080" << endl;
+        config_file << "volume=100" << endl;
         config_file.close();
     }
 
     else
-        assert("Can't open config file, please check if the program is able to write into the directory");
+        cout << "Can't open config file, please check if the program is able to write into the directory" << endl;
 }
 
 void Config::readConfig()
 {
-    ifstream config_file;
+    ifstream config_file(filename, ios::in);
 
     if(config_file.is_open())
     {
-        // TODO : Vérifier l'état de tous les paramètres dans le fichier et mettre à jour les attributs
+        string line;
+
+        while(getline(config_file, line))
+        {
+            string key, value;
+            key = line.substr(0, line.find("="));
+            value = line.substr(line.find("=")+1, line.size());
+            config_map[key] = value;
+        }
+
         config_file.close();
     }
 
     else
         createConfig();
+}
+
+string Config::getSetting(string key)
+{
+    if(config_map.find(key) != config_map.end())
+        return config_map[key];
+
+    return "";
 }
--- src/config.h
+++ src/config.h
@@ -4,17 +4,20 @@
 #include <iostream>
 #include <fstream>
 #include <cassert>
+#include <map>
 
 class Config
 {
 private:
     std::string filename;
+    std::map<std::string, std::string> config_map;
 
 public:
     Config(std::string filename);
     ~Config();
     void createConfig();
     void readConfig();
+    std::string getSetting(std::string key);
 };
 
 #endif
