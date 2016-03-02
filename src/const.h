3edaed0c20bd90c12b00a21c12f69ca44b306de9
Clement Fleury <clement.fleury@etu.u-bordeaux.fr>
2016-03-02 23:48:33 +0100

    Les chemins d'accès aux ressources sont simplifiés

--- src/const.h
+++ src/const.h
@@ -1,9 +1,9 @@
 #ifndef CONST_H
 #define CONST_H
 
-#define BUTTON_MENU_IMG "../Runner/img/button.png"
-#define MENU_BG "../Runner/img/menu_bg.png"
-#define ONTHEMOVE_TTF "../Runner/fonts/onthemove.ttf"
-#define SPLASH_IMG "../Runner/img/splash.jpg"
+#define BUTTON_MENU_IMG "img/button.png"
+#define MENU_BG "img/menu_bg.png"
+#define ONTHEMOVE_TTF "fonts/onthemove.ttf"
+#define SPLASH_IMG "img/splash.jpg"
 
 #endif // CONST_H
