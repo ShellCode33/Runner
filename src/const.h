4d20ce4d60b4808644923ef10895b75aa9b10ef5
jerryPoppy <jeremie.47@live.fr>
2016-03-03 17:49:16 +0100

    ajout gestion de l'animation et de l'animation des sprites

--- src/const.h
+++ src/const.h
@@ -4,6 +4,6 @@
 #define BUTTON_MENU_IMG "img/button.png"
 #define MENU_BG "img/menu_bg.png"
 #define ONTHEMOVE_TTF "fonts/onthemove.ttf"
-#define SPLASH_IMG "img/splash.jpg"
+#define SPLASH_IMG "img/splash.png"
 
 #endif // CONST_H
