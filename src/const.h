71628ed289b754881d255b02beae347c069dfa1c
jerry <jerry@localhost.localdomain>
2016-03-25 05:35:28 +0100

    modification du splashscreen

--- src/const.h
+++ src/const.h
@@ -19,14 +19,14 @@
 
 //STRING
 #define TITLE_WINDOW "Super Weed Boy"
-#define SPLASH_TEXT "<< Press Space To Play >>"
+#define SPLASH_TEXT "<< Press Space >>"
 
 //PATH
 #define CURSOR_IMG "img/cursor.png"
 #define BUTTON_MENU_IMG "img/button.png"
 #define MENU_BG "img/menu_bg.png"
 #define ONTHEMOVE_TTF "fonts/onthemove.ttf"
-#define SPLASH_IMG "img/splash2.png"
+#define SPLASH_IMG "img/splash3.png"
 #define PLAYER_IMG "img/player.png"
 #define SNOOP_DOGG_ANIM "img/snoop.png"
 #define SNOOP_DOGG_ANIM_REVERSE "img/snoop_reverse.png"
--- src/window.cpp
+++ src/window.cpp
@@ -34,7 +34,8 @@ void WindowRunner::create()
     this->window->setView(*this);
     this->state = SPLASH;
 
-    this->splash_screen.setTextPosition(Vector2f((this->getSize().x - this->splash_screen.getTextWidth()) / 2, (this->getSize().y - this->splash_screen.getTextHeight()) / 1.2));
+    //this->splash_screen.setTextPosition(Vector2f((this->getSize().x - this->splash_screen.getTextWidth()) / 2, (this->getSize().y - this->splash_screen.getTextHeight()) / 1.2));
+    this->splash_screen.setTextPosition(Vector2f((this->getSize().x / 50), (this->getSize().y) / 1.6));
 
     this->window->setFramerateLimit(FRAMERATE);
     this->window->setVerticalSyncEnabled(false);
