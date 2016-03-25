f3747df78d601ff31181d1e8a7d9eb1705b95f7e
jerry <jerry@localhost.localdomain>
2016-03-25 07:40:13 +0100

    positionnement du tete du splash

--- src/window.cpp
+++ src/window.cpp
@@ -35,7 +35,7 @@ void WindowRunner::create()
     this->state = SPLASH;
 
     //this->splash_screen.setTextPosition(Vector2f((this->getSize().x - this->splash_screen.getTextWidth()) / 2, (this->getSize().y - this->splash_screen.getTextHeight()) / 1.2));
-    this->splash_screen.setTextPosition(Vector2f((this->getSize().x / 50), (this->getSize().y) / 1.6));
+    this->splash_screen.setTextPosition(Vector2f((this->getSize().x / ((2 * FRAMERATE) / 3)), (this->getSize().y) / 1.75));
 
     this->window->setFramerateLimit(FRAMERATE);
     this->window->setVerticalSyncEnabled(false);
