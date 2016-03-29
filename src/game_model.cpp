3a3fd8da1b9bff9724fd3d6638d7d3c286a3ab7e
Clement Fleury <clement.fleury@etu.u-bordeaux.fr>
2016-03-29 23:10:31 +0200

    Debut avancement du feu

--- src/game_model.cpp
+++ src/game_model.cpp
@@ -3,9 +3,10 @@
 
 using namespace std;
 
-GameModel::GameModel() : score(0)
+GameModel::GameModel() : score(0), fire_offset(0), fire_speed(3), time_per_move(30)
 {
     this->game_begin = chrono::system_clock::now();
+    this->timer = chrono::system_clock::now();
 }
 
 GameModel::~GameModel()
@@ -17,11 +18,31 @@ void GameModel::update()
 {
     auto diff = chrono::system_clock::now() - this->game_begin;
     auto msec = chrono::duration_cast<chrono::milliseconds>(diff);
-
     this->score = msec.count() / 100;
+
+
+    auto diff2 = chrono::system_clock::now() - this->timer;
+    auto msec2 = chrono::duration_cast<chrono::milliseconds>(diff2);
+
+    if(msec2.count() > this->time_per_move)
+    {
+        this->fire_offset = this->fire_speed;
+        this->timer = chrono::system_clock::now();
+    }
+
+    else
+    {
+        this->fire_offset = 0;
+    }
+
 }
 
 unsigned long GameModel::getScore() const
 {
     return this->score;
+}
+
+int GameModel::getFireOffset() const
+{
+    return fire_offset;
 }
