9359704de353a80b066e20bc3ed4df3648d4a15d
Jeremie Pereyrol <jpereyrol@info-towensx.iut.bx1>
2016-04-29 16:33:24 +0200

    commentaire

--- src/coin_model.h
+++ src/coin_model.h
@@ -1,3 +1,10 @@
+/*!
+ * \file coin_model.h
+ * \class CoinModel
+ * \brief Partie modèle des pièces
+ * \author Jérémie
+ */
+
 #ifndef COIN_MODEL_H
 #define COIN_MODEL_H
 
@@ -6,11 +13,32 @@
 class CoinModel : public Obstacle
 {
 public:
+    /*!
+     * \brief CoinModel construceur
+     * \param relat_x
+     * \param relat_y
+     * \param width
+     * \param height
+     */
     CoinModel(int relat_x, int relat_y, int width, int height);
     ~CoinModel();
 
+    /*!
+     * \brief action qui s'applique lorsque le playerModel entre en collision avec la pièce
+     * \param game
+     */
     void action(GameModel &game);
+
+    /*!
+     * \brief update
+     */
     void update();
+
+    /*!
+     * \brief checkCollision
+     * \param m
+     * \return true ou false suivant si la collision a lieu ou pas
+     */
     bool checkCollision(Movable &m) const;
 };
 
